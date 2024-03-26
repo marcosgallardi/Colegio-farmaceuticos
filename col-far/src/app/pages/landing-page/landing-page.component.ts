d99bb00c20ce1aab05c373ae57a26e8deec0ffd9
marcosgallardi <gallardimarcos98@gmail.com>
2024-03-26 17:24:48 -0300

    creacion del componente datos

--- col-far/src/app/pages/landing-page/landing-page.component.ts
+++ col-far/src/app/pages/landing-page/landing-page.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { NosotrosComponent } from '../../components/nosotros/nosotros.component';
+import { DatosComponent } from '../../components/datos/datos.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [NavbarComponent,NosotrosComponent],
+  imports: [NavbarComponent, NosotrosComponent, DatosComponent],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css'
+  styleUrl: './landing-page.component.css',
 })
-export class LandingPageComponent {
-
-}
+export class LandingPageComponent {}
