cfca0a8e7a9cb543c2ea2e0d3a96a5c2696f15e3
marcosgallardi <gallardimarcos98@gmail.com>
2024-03-27 17:18:15 -0300

    finalizacion del componente nosotros en movil y desktop y el componente datos

--- col-far/src/app/components/datos/datos.component.ts
+++ col-far/src/app/components/datos/datos.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { TweenMax } from 'gsap';
 
 @Component({
   selector: 'app-datos',
   standalone: true,
   imports: [],
   templateUrl: './datos.component.html',
-  styleUrl: './datos.component.css'
+  styleUrl: './datos.component.css',
 })
-export class DatosComponent {
 
+export class DatosComponent implements OnInit {
+  @ViewChild('datosContainer', { static: true }) datosContainer!: ElementRef;
+  isVisible: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.isVisible = true;
+          this.animateNumbers();
+          observer.unobserve(entry.target);
+        }
+      });
+    });
+
+    observer.observe(this.datosContainer.nativeElement);
+  }
+
+  animateNumbers(): void {
+    const items = this.datosContainer.nativeElement.querySelectorAll('.item-dato h4');
+    items.forEach((item: HTMLElement, index: number) => {
+      const finalValue = parseInt(item.innerText.trim(), 10);
+      item.innerText = '0';
+
+      TweenMax.to(item, 4, {
+        delay: index * 0.5,
+        textContent: finalValue.toString(),
+        roundProps: 'textContent',
+        ease: 'Power1.easeOut',
+      });
+    });
+  }
 }
