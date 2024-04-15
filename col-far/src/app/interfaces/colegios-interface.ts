02ce9cd83363049edfefbb87a9790a7d7b3d0132
marcosgallardi <gallardimarcos98@gmail.com>
2024-04-15 15:50:55 -0300

    avanzando en footer

--- col-far/src/app/interfaces/colegios-interface.ts
+++ col-far/src/app/interfaces/colegios-interface.ts
@@ -4,6 +4,9 @@ export interface Colegios {
   logo: string;
   nosotros: string;
   nosotrosImg: string;
+  direccion: string;
+  telefono: string;
+  email: string;
   cantFarm: number;
   cantProf: number;
   cantHab: number;
