8ce044520210dad26ad70b672aec09080d2e9846
marcosgallardi <gallardimarcos98@gmail.com>
2024-04-17 11:54:34 -0300

    avanzando en la seccion institucional

--- col-far/src/app/pages/institucional-page/institucional-page/institucional-page.component.ts
+++ col-far/src/app/pages/institucional-page/institucional-page/institucional-page.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../../components/navbar/navbar.component';
+import { NosotrosComponent } from '../../../components/nosotros/nosotros.component';
+import { FooterComponent } from '../../../components/footer/footer.component';
 
 @Component({
   selector: 'app-institucional-page',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, NosotrosComponent, FooterComponent],
   templateUrl: './institucional-page.component.html',
-  styleUrl: './institucional-page.component.css'
+  styleUrl: './institucional-page.component.css',
 })
-export class InstitucionalPageComponent {
-
-}
+export class InstitucionalPageComponent {}
