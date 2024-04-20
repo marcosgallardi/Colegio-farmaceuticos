d7ea87a6c777c3e242892e03197f8a491597f121
marcosgallardi <gallardimarcos98@gmail.com>
2024-04-20 11:12:26 -0300

    finalizacion de la seccion famrmacias

--- col-far/src/app/pages/farmacias-page/farmacias-page.component.ts
+++ col-far/src/app/pages/farmacias-page/farmacias-page.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { TablaFarmaciasComponent } from '../../components/tabla-farmacias/tabla-farmacias.component';
+import { BannerTitleComponent } from '../../components/banner-title/banner-title.component';
 
 @Component({
   selector: 'app-farmacias-page',
   standalone: true,
-  imports: [TablaFarmaciasComponent],
+  imports: [TablaFarmaciasComponent,BannerTitleComponent],
   templateUrl: './farmacias-page.component.html',
-  styleUrl: './farmacias-page.component.css'
+  styleUrl: './farmacias-page.component.css',
 })
-export class FarmaciasPageComponent {
-
-}
+export class FarmaciasPageComponent {}
--- col-far/src/app/pages/institucional-page/institucional-page/institucional-page.component.ts
+++ col-far/src/app/pages/institucional-page/institucional-page/institucional-page.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NavbarComponent } from '../../../components/navbar/navbar.component';
 import { NosotrosComponent } from '../../../components/nosotros/nosotros.component';
 import { FooterComponent } from '../../../components/footer/footer.component';
 import { ComisionDirectivaComponent } from '../../../components/comision-directiva/comision-directiva.component';
+import { BannerTitleComponent } from '../../../components/banner-title/banner-title.component';
 
 @Component({
   selector: 'app-institucional-page',
@@ -12,8 +13,11 @@ import { ComisionDirectivaComponent } from '../../../components/comision-directi
     NosotrosComponent,
     FooterComponent,
     ComisionDirectivaComponent,
+    BannerTitleComponent
   ],
   templateUrl: './institucional-page.component.html',
   styleUrl: './institucional-page.component.css',
 })
-export class InstitucionalPageComponent {}
+export class InstitucionalPageComponent {
+  
+}
