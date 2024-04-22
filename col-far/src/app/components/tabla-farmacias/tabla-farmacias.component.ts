9918b6566059a73b8cc17c124f0d8611ee9f2c64
marcosgallardi <gallardimarcos98@gmail.com>
2024-04-22 16:01:59 -0300

    avanzando

--- col-far/src/app/components/tabla-farmacias/tabla-farmacias.component.ts
+++ col-far/src/app/components/tabla-farmacias/tabla-farmacias.component.ts
@@ -1,12 +1,67 @@
 import { Component } from '@angular/core';
+import { Farmacias } from '../../interfaces/farmacias-interface';
 
 @Component({
   selector: 'app-tabla-farmacias',
   standalone: true,
   imports: [],
   templateUrl: './tabla-farmacias.component.html',
-  styleUrl: './tabla-farmacias.component.css'
+  styleUrl: './tabla-farmacias.component.css',
 })
 export class TablaFarmaciasComponent {
-
+  farmacias: Farmacias[] = [
+    {
+      id: 1,
+      nombre: 'Gonzales',
+      localidad: 'mercedes',
+      direccion: 'calle 24 N° 658',
+      email: 'XXXXXXXXXXXXXXXXXX',
+      caracteristica: 2324,
+      telefono: 461486,
+      whatsapp: 502670,
+    },
+    {
+      id: 2,
+      nombre: 'Rubiero',
+      localidad: 'mercedes',
+      direccion: 'calle 24 N° 658',
+      email: 'XXXXXXXXXXXXXXXXXX',
+      caracteristica: 2324,
+      telefono: 461486,
+      whatsapp: 502670,
+    },
+    {
+      id: 3,
+      nombre: 'Gonzales',
+      localidad: 'mercedes',
+      direccion: 'calle 24 N° 658',
+      email: 'XXXXXXXXXXXXXXXXXX',
+      caracteristica: 2324,
+      telefono: 461486,
+      whatsapp: 502670,
+    },
+    {
+      id: 4,
+      nombre: 'Gonzales',
+      localidad: 'mercedes',
+      direccion: 'calle 24 N° 658',
+      email: 'XXXXXXXXXXXXXXXXXX',
+      caracteristica: 2324,
+      telefono: 461486,
+      whatsapp: 502670,
+    },
+    {
+      id: 5,
+      nombre: 'Gonzales',
+      localidad: 'mercedes',
+      direccion: 'calle 24 N° 658',
+      email: 'XXXXXXXXXXXXXXXXXX',
+      caracteristica: 2324,
+      telefono: 461486,
+      whatsapp: 502670,
+    },
+  ];
+  filterFarmacia({ value }: any) {
+    return this.farmacias.includes(value);
+  }
 }
---
+++ col-far/src/app/interfaces/farmacias-interface.ts
@@ -0,0 +1,10 @@
+export interface Farmacias {
+  id: number;
+  nombre: string;
+  localidad: string;
+  direccion: string;
+  email?: string;
+  caracteristica?: number;
+  telefono: number;
+  whatsapp: number;
+}
