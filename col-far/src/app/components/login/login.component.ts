e1be25a96500255ff989e81e0455455652fc2d41
marcosgallardi <gallardimarcos98@gmail.com>
2024-05-22 13:19:52 -0300

    avanzando

--- col-far/src/app/components/login/login.component.ts
+++ col-far/src/app/components/login/login.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Colegios } from '../../interfaces/colegios-interface';
+import { ColegiosService } from '../../core/colegios.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit {
+  public colegio$!: Observable<Colegios>;
+  constructor(private service: ColegiosService) {}
+  ngOnInit(): void {
+    this.colegio$ = this.service.getColegios();
+  }
 }
