d4e3f0d4cb6fa14474294cd9ed00fc2e1503ef0e
marcosgallardi <gallardimarcos98@gmail.com>
2024-05-28 11:58:57 -0300

    comenzando en dashboard

---
+++ col-far/src/app/components/nav-dashboard/nav-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-dashboard.component.html',
+  styleUrl: './nav-dashboard.component.css'
+})
+export class NavDashboardComponent {
+
+}
--- col-far/src/app/pages/dashboard/dashboard/dashboard.component.ts
+++ col-far/src/app/pages/dashboard/dashboard/dashboard.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { NavDashboardComponent } from '../../../components/nav-dashboard/nav-dashboard.component';
+import { SidebarComponent } from '../../../components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [NavDashboardComponent, SidebarComponent],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}
