45338c3367dc5346e019f13395285ca7fd4c81a7
marcosgallardi <gallardimarcos98@gmail.com>
2024-05-30 11:50:06 -0300

    avanzando en el dashboard

--- col-far/src/app/components/sidebar/sidebar.component.ts
+++ col-far/src/app/components/sidebar/sidebar.component.ts
@@ -1,18 +1,52 @@
 import { Component } from '@angular/core';
 import { ItemsSideComponent } from '../items-side/items-side.component';
 import { AsyncPipe } from '@angular/common';
+import { itemSide } from '../../interfaces/items-side-interface';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [ItemsSideComponent,AsyncPipe],
+  imports: [ItemsSideComponent, AsyncPipe],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
-  itemSides = {
-    name: 'vergero',
-    icon: 'bi bi-house',
-    dropdown: false,
-  };
+  itemSides: itemSide[] = [
+    {
+      name: 'Colegio',
+      icon: 'bi bi-house',
+      dropdown: true,
+    },
+    {
+      name: 'Usuarios',
+      icon: 'bi bi-people',
+      dropdown: true,
+    },
+    {
+      name: 'Novedades',
+      icon: 'bi bi-window-stack',
+      dropdown: false,
+    },
+    {
+      name: 'Farmacias',
+      icon: 'bi bi-shop',
+      dropdown: false,
+    },
+    {
+      name: 'Contacto',
+      icon: 'bi bi-telephone',
+      dropdown: false,
+    },
+    {
+      name: 'Turnos',
+      icon: 'bi bi-layout-text-sidebar-reverse',
+      dropdown: false,
+    },
+    {
+      name: 'Datos',
+      icon: 'bi bi-bar-chart-line',
+      dropdown: false,
+    },
+    
+  ];
 }
--- col-far/src/app/helpers/endpoints/server.ts
+++ col-far/src/app/helpers/endpoints/server.ts
@@ -1,2 +1,2 @@
-export const server = "http://localhost:3000"
+export const server = "http://localhost:4001"
 export const server1 = "http://localhost:4000"
