106021667d490bfdbc06352fa667d574a16cfc76
marcosgallardi <gallardimarcos98@gmail.com>
2024-06-08 10:19:28 -0300

    mod turnos avanzando

--- col-far/src/app/components/modifications/turnos/turnos.component.ts
+++ col-far/src/app/components/modifications/turnos/turnos.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { TurnosFarmComponent } from '../../turnos-farm/turnos-farm.component';
+import { ButtonMainComponent } from '../../button-main/button-main.component';
 
 @Component({
   selector: 'app-turnos',
   standalone: true,
-  imports: [],
+  imports: [TurnosFarmComponent,ButtonMainComponent],
   templateUrl: './turnos.component.html',
-  styleUrl: './turnos.component.css'
+  styleUrl: './turnos.component.css',
 })
-export class TurnosComponent {
-
-}
+export class TurnosComponent {}
