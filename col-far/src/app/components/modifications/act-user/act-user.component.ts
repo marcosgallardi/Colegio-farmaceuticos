c53da84a962c26997e317682780896eda2682b2b
marcosgallardi <gallardimarcos98@gmail.com>
2024-06-23 11:55:58 -0300

    fase final de modificaciones

--- col-far/src/app/components/modifications/act-user/act-user.component.ts
+++ col-far/src/app/components/modifications/act-user/act-user.component.ts
@@ -6,37 +6,45 @@ import { User } from '../../../interfaces/user-interface';
   standalone: true,
   imports: [],
   templateUrl: './act-user.component.html',
-  styleUrl: './act-user.component.css'
+  styleUrl: './act-user.component.css',
 })
 export class ActUserComponent {
-  users:User[] =[
+  users: User[] = [
     {
-      id:1,
-      name:"Marcos gallardi",
-      password:"123",
-      token:"123",
-      role:"Admin"
+      id: 1,
+      name: 'Marcos gallardi',
+      userName:'pepito',
+      password: '123',
+      token: '123',
+      role: 'Admin',
     },
     {
-      id:2,
-      name:"Elvio Lento",
-      password:"1233",
-      token:"12334",
-      role:"Superadmin"
+      id: 2,
+      name: 'Elvio Lento',
+      userName:'pepito',
+      password: '1233',
+      token: '12334',
+      role: 'Superadmin',
     },
     {
-      id:3,
-      name:"Lionel messi",
-      password:"1235",
-      token:"1235",
-      role:"Admin"
+      id: 3,
+      name: 'Lionel messi',
+      userName:'pepito',
+      password: '1235',
+      token: '1235',
+      role: 'Admin',
     },
     {
-      id:4,
-      name:"Javier milei",
-      password:"1237",
-      token:"1238",
-      role:"Admin"
+      id: 4,
+      name: 'Javier milei',
+      userName:'pepito',
+      password: '1237',
+      token: '1238',
+      role: 'Admin',
     },
-  ]
+  ];
+  onClickButtons(id: number) {
+  
+
+  }
 }
